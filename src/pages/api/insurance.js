dcec1b1d0dbc0f43856faaf1020493e93fe3264d
ranaharoon3222 <ranaharoon3222@gmail.com>
2023-11-24 14:58:14 +0500

    fixed item

--- src/pages/api/insurance.js
+++ src/pages/api/insurance.js
@@ -30,39 +30,28 @@ export default async function handler(req, res) {
   await runMiddleware(req, res, cors);
   if (req.method !== 'POST') return res.status(200).json({ name: 'Hi AFS' });
 
-  // const updateProduct = JSON.parse(req?.body);
+  const cartValue = JSON?.parse(req?.body);
 
-  // const url = `https://all-fresh-seafood.myshopify.com/admin/api/2023-01/products/8256097845481.json`;
   try {
-    // const productResponse = await fetch(url, {
-    //   method: 'PUT',
-    //   headers: {
-    //     'Content-Type': 'application/json',
-    //     'X-Shopify-Access-Token': process.env.TOKEN,
-    //   },
-    //   body: JSON.stringify(updateProduct),
-    // });
+    const allVariantsUrl = `https://all-fresh-seafood.myshopify.com/admin/api/2023-01/products/8256097845481/variants.json`;
 
-    const draftUrl = `https://all-fresh-seafood.myshopify.com/admin/api/2023-01/draft_orders.json`;
-
-    const draftOrder = await fetch(draftUrl, {
-      method: 'POST',
+    const fetchAllVaraints = await fetch(allVariantsUrl, {
+      method: 'GET',
       headers: {
         'Content-Type': 'application/json',
         'X-Shopify-Access-Token': process.env.TOKEN,
       },
-      body: JSON.stringify({
-        draft_order: {
-          line_items: [
-            { title: 'Custom product', price: '20.00', quantity: 1 },
-          ],
-        },
-      }),
     });
 
-    const orderRes = await draftOrder.json();
+    const allVaraints = await fetchAllVaraints.json();
 
-    res.status(200).json(orderRes);
+    const findVariant = allVaraints.variants.find((item) => {
+      if ((cartValue.value / 100) * 3 <= item.price) {
+        return item;
+      }
+    });
+
+    res.status(200).json(findVariant);
   } catch (error) {
     console.error(error);
     res.status(400).json(error);
